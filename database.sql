f7c03d604a18d4f9b1c2ae34482518efb33ba328
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-14 00:01:11 +0200

    Create table Trainings

--- database.sql
+++ database.sql
@@ -7,3 +7,20 @@ CREATE TABLE Users(
 );
 
 --@block
+CREATE TABLE Trainings(
+    id INT auto_increment PRIMARY KEY,
+    training_title VARCHAR(255) NOT NULL,
+    training_start_date DATE,
+    training_end_date DATE,
+    training_start_time TIME,
+    training_end_time TIME,
+    training_language VARCHAR(50) NOT NULL,
+    training_description TEXT,
+    training_level VARCHAR(20) NOT NULL,
+    training_category VARCHAR(255) NOT NULL,
+    training_location VARCHAR(100) NOT NULL,
+    trainer_id INT NOT NULL,
+    FOREIGN KEY (trainer_id) REFERENCES Users(id)
+);
+
+--@block
