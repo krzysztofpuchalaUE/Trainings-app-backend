7b0b75bda789abf9a4e29a7c1fd651caecf99a46
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-15 01:21:13 +0200

    Add post method in /user-trainings/new-training route

--- routes/trainings.js
+++ routes/trainings.js
@@ -25,3 +25,36 @@ router.route("/user-trainings").get(async (req, res) => {
   const myTrainings = await queries.getAllUserTrainings();
   res.json(myTrainings);
 });
+
+router.post("/user-trainings/new-training", async (req, res) => {
+  const {
+    title,
+    category,
+    startDate,
+    endDate,
+    startTime,
+    endTime,
+    language,
+    location,
+    description,
+    level,
+    trainerId,
+    iconUrl,
+  } = req.body.data;
+  console.log(req.body.data);
+  const training = await queries.createTraining(
+    title,
+    category,
+    startDate,
+    endDate,
+    startTime,
+    endTime,
+    language,
+    location,
+    description,
+    level,
+    trainerId,
+    iconUrl
+  );
+  res.send(training);
+});
