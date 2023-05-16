0fa847b942c0f2a6a66390164a9ae8f91b7b9e83
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-16 21:40:12 +0200

    Add post methods in /login and /signin routes in new file auth.js

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+import * as queries from "../database.js";
+import express from "express";
+import bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+
+export const router = express.Router();
+
+router.post("/signin", async (req, res) => {
+  console.log("body:", req.body);
+  const { firstName, lastName, username, email } = req.body.data;
+
+  const hashedPassword = bcrypt.hash(req.body.password, 10);
+  const registerUser = await queries.registerUser(
+    firstName,
+    lastName,
+    username,
+    email,
+    hashedPassword
+  );
+  res.send(registerUser);
+
+  // const token = createJson;
+});
+
+router.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userPassword = queries.loginUser(email);
+
+  try {
+    if (bcrypt.compare(password, userPassword)) {
+      res.send("Success");
+    } else {
+      res.send("Not allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
