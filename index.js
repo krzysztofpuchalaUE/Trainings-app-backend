db27bfbd03393218648633becc71f0d0fa43f9d7
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-21 22:08:12 +0200

    Change server.js to index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const app = express();
+app.use("/uploads", express.static("uploads"));
+import { router as allTrainingsRouter } from "./routes/trainings.js";
+import { router as authRoutes } from "./routes/auth.js";
+
+app.use(cors());
+app.use(express.json());
+const trainingsRouter = allTrainingsRouter;
+const authRouter = authRoutes;
+
+app.use(trainingsRouter);
+app.use(authRouter);
+const PORT = process.env.PORT;
+
+app.listen(PORT || 8800);
