efc3c71cd906462863c58987fe353abf6f805dba
Krzysztof Puchała <krzysztof.puchala@edu.uekat.pl>
2023-05-21 22:53:56 +0200

    move cors up in index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 import { router as allTrainingsRouter } from "./routes/trainings.js";
 import { router as authRoutes } from "./routes/auth.js";
 
-app.use(cors());
-app.use(express.json());
 const trainingsRouter = allTrainingsRouter;
 const authRouter = authRoutes;
 
